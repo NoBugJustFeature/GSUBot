2fc15fca6ffaec5b428aaead2bca889fda0f31bd
NoBugJistFeature <maks_vinokurov_2005@mail.ru>
2023-02-22 11:57:15 +0300

    update admin panel

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,5 @@
 from .start import dp
 from .help import dp
-from .admin import dp
 from .testing import dp
 
 from .database_handlers import dp
--- handlers/start.py
+++ handlers/start.py
@@ -3,7 +3,7 @@ from loader import dp
 
 from filters import IsAdmin
 from keyboards.user_keyboards.keyboard_start import kb_start
-from keyboards.admin_keyboards.Keyboard_start_admin import kb_admin_start
+from keyboards.admin_keyboards.keyboard_admin import kb_admin
 
 from utils.misc import rate_limit
 from loader import await_time
@@ -11,9 +11,8 @@ from loader import await_time
 
 @dp.message_handler(IsAdmin(), text="/start")
 async def command_start(message: Message):
-    await message.answer(f"Здравствуйте, {message.from_user.first_name}\n"
-                           "Напишите /admin чтобы войти в панель администратора",
-                          reply_markup=kb_admin_start)
+    await message.answer(f"Здравствуйте, {message.from_user.first_name}",
+                          reply_markup=kb_admin)
     
 
 @rate_limit(limit=await_time, key="/start")
