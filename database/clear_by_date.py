5a92fc66a99a886ed3039c7d5fcf3ea9bba3ef28
NoBugJistFeature <maks_vinokurov_2005@mail.ru>
2023-02-22 11:59:48 +0300

    updated the database cleanup command

--- database/clear_by_date.py
+++ database/clear_by_date.py
@@ -2,4 +2,4 @@ from loader import connection
 
 async def clear_by_date_db(date):
     with connection.cursor() as cur:
-        cur.execute("DELETE FROM list WHERE date = '{}'".format(date))
+        cur.execute("DELETE FROM list WHERE date = %s", (date))
