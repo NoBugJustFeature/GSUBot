ed9370793a3c6638b1a3e563929c2883f7564e62
NoBugJistFeature <maks_vinokurov_2005@mail.ru>
2023-02-24 17:11:52 +0300

    hotfix

--- database/clear_by_date.py
+++ database/clear_by_date.py
@@ -1,5 +1,5 @@
 from loader import connection
 
-async def clear_by_date_db(date):
+def clear_by_date_db(date):
     with connection.cursor() as cur:
-        cur.execute("DELETE FROM list WHERE date = %s", (date))
+        cur.execute("DELETE FROM list WHERE date = (%s)", (date,))
